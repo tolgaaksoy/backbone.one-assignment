d6699bc7ac07c322df7017b677611963a1e9a487
Tolga Aksoy <dev.tolgaaksoy@gmail.com>
2023-07-24 12:25:40 +0300

    Add(project): exceptions

---
+++ src/main/java/one/backbone/messagingassignment/exception/UserManagementExceptionHandler.java
@@ -0,0 +1,47 @@
+package one.backbone.messagingassignment.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import one.backbone.messagingassignment.model.dto.response.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.Instant;
+
+/**
+ * The type User management exception handler.
+ */
+@Slf4j
+@RestControllerAdvice
+public class UserManagementExceptionHandler {
+
+    /**
+     * Handle t user not found exception base response.
+     *
+     * @param ex      the ex
+     * @param request the request
+     * @return the base response
+     */
+    @ExceptionHandler(value = UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public BaseResponse handleTUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+        return BaseResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .timestamp(Instant.now())
+                .build();
+    }
+
+    @ExceptionHandler(value = UsernameAlreadyExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public BaseResponse handleUsernameAlreadyExistException(UsernameAlreadyExistException ex, WebRequest request) {
+        return BaseResponse.builder()
+                .status(HttpStatus.CONFLICT.value())
+                .message(ex.getMessage())
+                .timestamp(Instant.now())
+                .build();
+    }
+
+}
